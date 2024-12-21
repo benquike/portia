ae675556d3352be5799b915b4a1a3c7b5bdf335e
Hui Peng <benquike@gmail.com>
2024-12-21 02:13:20 -0800

    fix django url
    Signed-off-by: Hui Peng <benquike@gmail.com>

--- portia_server/portia_api/urls.py
+++ portia_server/portia_api/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import path as url
 
 from .routers import Router, NestedRouter
 from .resources.annotations import AnnotationRoute
--- portia_server/portia_server/urls.py
+++ portia_server/portia_server/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import path as url
 
 from . import views
 from portia_api import urls
